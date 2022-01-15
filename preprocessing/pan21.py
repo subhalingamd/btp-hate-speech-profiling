e911f0761289db2a9dcdbeeaadcbf5e92038a81e
Subhalingam D <subhalingam.d@gmail.com>
2022-01-15 23:41:41 +0530

    preprocess pan21

---
+++ preprocessing/pan21.py
@@ -0,0 +1,46 @@
+import re
+import xml.etree.ElementTree as ET
+
+def parse_user_file(path):
+    # parse file in path
+    tree = ET.parse(path)
+    root = tree.getroot()
+
+    # get texts inside "./documents/document"
+    texts = [doc.text for doc in root.findall('./documents/document')]
+    
+    # remove "RT #USER#: " from the beginning of each text
+    texts = [re.sub(r'^RT #USER#: ', '', text) for text in texts]
+
+    return texts
+
+def parse_labels_file(path):
+    # open file in path
+    with open(path, 'r') as f:
+        # read all lines
+        lines = f.readlines()
+
+    # split at ':::'
+    lines = [line.split(':::') for line in lines]
+    lines = [(user, int(label)) for user, label in lines]
+
+    return lines
+
+def read_dataset(data_dir):
+    # get gold values from data_dir/labels.txt
+    labels = parse_labels_file(data_dir + '/labels.txt')
+
+    # define data: {label: [tweets]}
+    data = {}
+
+    # for each labels
+    for user, label in labels:
+        # get user's tweets from data_dir/user.xml
+        tweets = parse_user_file(data_dir + '/' + str(user) + '.xml')
+
+        # append tweets to data 
+        if label not in data:
+            data[label] = []
+        data[label].append(tweets)
+
+    return data
---
+++ run_tests.py
@@ -0,0 +1,1 @@
+from tests.preprocessing import *
