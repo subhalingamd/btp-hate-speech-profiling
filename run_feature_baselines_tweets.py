8ce5592149369f45e706cf9b95878dadf2d55163
Subhalingam D <subhalingam.d@gmail.com>
2022-03-02 20:01:31 +0530

    [IMP] update feature baseline for tweets

--- run_feature_baselines_tweets.py
+++ run_feature_baselines_tweets.py
@@ -2,7 +2,7 @@ from argparse import ArgumentParser
 
 def parse_args():
     parser = ArgumentParser(description='Running baselines')
-    parser.add_argument('--data_path', '-d', type=str, default='inputs/tweets/hs+hateval2019_en_train__processed.tsv, help='data path')
+    parser.add_argument('--data_path', '-d', type=str, default='inputs/tweets/hs+hateval2019_en_train__processed.tsv', help='data path')
     parser.add_argument('--classifier', '-c', type=str, default='lr', choices=['lr', 'svm', 'nb', 'rf', ], help='classifier')
     parser.add_argument('--output_dir', '-o', type=str, default='outputs/feature_baselines_tweets/hs+hateval2019', help='output directory')
     parser.add_argument('--best_metrics_file', '-b', type=str, default='outputs/feature_baselines_tweets/hs+hateval2019/best_metrics.tsv', help='best metrics file')
@@ -15,16 +15,16 @@ if __name__ == '__main__':
     args = parse_args()
     
     if args.mode == 'train':
-        if "test" in args.merged_data:
+        if "test" in args.data_path:
             raise ValueError("Using test data for training is not allowed.")
         args.output_dir = args.output_dir.rstrip('/')
         out_prefix = "{}/{}".format(args.output_dir, args.classifier)
         from models.tweets.feature_baselines import train
-        train(data=args.features_data, cls=args.classifier, dump_objects_to=out_prefix+'.pkl', store_params_to=out_prefix+'.tsv')
+        train(data=args.data_path, cls=args.classifier, dump_objects_to=out_prefix+'.pkl', store_params_to=out_prefix+'.tsv')
     
     elif args.mode == 'test':
-        if "train" in args.features_data:
+        if "train" in args.data_path:
             raise ValueError("Using train data for testing is not allowed.")
         out_prefix = "{}/test_".format(args.output_dir)
         from models.tweets.feature_baselines import test
-        test(data=args.features_data, model_path=args.model_path, store_scores_to=out_prefix+'scores.tsv', store_predictions_to=None, include_all_params=True)
+        test(data=args.data_path, model_path=args.model_path, store_scores_to=out_prefix+'scores.tsv', store_predictions_to=None, include_all_params=True)
