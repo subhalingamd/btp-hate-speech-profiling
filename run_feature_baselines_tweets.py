b057e6adbcf21d26c88fe8848be2d60f93eaa153
Subhalingam D <subhalingam.d@gmail.com>
2022-03-02 19:00:43 +0530

    Create run_feature_baselines_tweets.py

---
+++ run_feature_baselines_tweets.py
@@ -0,0 +1,30 @@
+from argparse import ArgumentParser
+
+def parse_args():
+    parser = ArgumentParser(description='Running baselines')
+    parser.add_argument('--data_path', '-d', type=str, default='inputs/tweets/hs+hateval2019_en_train__processed.tsv, help='data path')
+    parser.add_argument('--classifier', '-c', type=str, default='lr', choices=['lr', 'svm', 'nb', 'rf', ], help='classifier')
+    parser.add_argument('--output_dir', '-o', type=str, default='outputs/feature_baselines_tweets/hs+hateval2019', help='output directory')
+    parser.add_argument('--best_metrics_file', '-b', type=str, default='outputs/feature_baselines_tweets/hs+hateval2019/best_metrics.tsv', help='best metrics file')
+    parser.add_argument('--model_path', '-m', type=str, default='outputs/feature_baselines_tweets/hs+hateval2019/rf.pkl', help='saved model path')
+    parser.add_argument('--mode', type=str, default='train', choices=['train', 'test'], help='train or test')
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    
+    if args.mode == 'train':
+        if "test" in args.merged_data:
+            raise ValueError("Using test data for training is not allowed.")
+        args.output_dir = args.output_dir.rstrip('/')
+        out_prefix = "{}/{}".format(args.output_dir, args.classifier)
+        from models.tweets.feature_baselines import train
+        train(data=args.features_data, cls=args.classifier, dump_objects_to=out_prefix+'.pkl', store_params_to=out_prefix+'.tsv')
+    
+    elif args.mode == 'test':
+        if "train" in args.features_data:
+            raise ValueError("Using train data for testing is not allowed.")
+        out_prefix = "{}/test_".format(args.output_dir)
+        from models.tweets.feature_baselines import test
+        test(data=args.features_data, model_path=args.model_path, store_scores_to=out_prefix+'scores.tsv', store_predictions_to=None, include_all_params=True)
