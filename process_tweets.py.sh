1765236a70d576c11cdd0abc7d243a276c20476c
Subhalingam D <subhalingam.d@gmail.com>
2022-03-21 19:50:00 +0530

    Create process_tweets.py.sh

---
+++ process_tweets.py.sh
@@ -0,0 +1,9 @@
+python process_tweets.py \
+    --data_path inputs/tweets/pan21_en_train_.tsv \
+    --output_dir inputs/tweets/ \
+    # --sampler None \
+
+python process_tweets.py \
+    --data_path inputs/tweets/pan21_en_test_.tsv \
+    --output_dir inputs/tweets/
+    
